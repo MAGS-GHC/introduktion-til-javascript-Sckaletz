56c3761bd8800dfab633afe9e67ab5d7f9e2ee8d
Sckaletz <allan@leeloo.dk>
2023-10-03 22:01:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,61 @@ console.log(
   "Opgave 1.1 - Lav et program, som skriver “Hello World” i konsollen"
 );
 //Lav jeres svar her
-console.log("XX");
+console.log("Hello World");
+
+// Opgave 1.2
+console.log("Hello\nWorld")
+
+// Opgave 1.3
+console.log("*\n**\n***\n**\n*")
+
+// Opgave 1.4
+let x = 5;
+console.log(typeof x)
+
+// Opgave 1.5
+console.log(10+5)
+console.log(25/3)
+console.log(25%3)
+console.log(25%2)
+console.log(3**2)
+
+// Opgave 1.6
+let tekst1 = "Hello "
+let tekst2 = "World!"
+console.log(tekst1+tekst2)
+
+// Opgave 1.7
+let tekst = "GF2 Online"
+console.log(tekst.length)
+
+// Opgave 1.8
+console.log()
+
+// Opgave 1.9
+console.log(Math.random())
+
+// Opgave 1.10
+let number = 23
+console.log(number) // viser tallet
+console.log(number = "23") // 23 er lig med 23
+console.log(number == "23") // bool (23 er lig med 23 = true)
+console.log(number === "23") // Tjekker om 23 er magen til 23
+console.log(number -= 23) // 23 - 23
+console.log(number += 23) // 23 + string 23
+console.log(number += "23") // Tjekker om 23 er magen til 23
+console.log(number -= "46") // 23 = 23 - 46
+console.log(number != "23") // 23 er ikke lig med string 23
+console.log(number **= 2) // 23 = 23 ** 2
+
+// Opgave 2.1
+let newnumber = 5
+
+if (newnumber % 2 == 0){
+  console.log("Et lige tal")
+} else{
+  console.log("Et ulige tal")
+}
+
+// Opgave 2.2
+
