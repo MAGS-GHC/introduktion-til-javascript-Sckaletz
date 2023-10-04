fdb330827f724b6bddfbfddd6af9b3d0747665e5
Sckaletz <allan@leeloo.dk>
2023-10-04 11:09:09 +0200

    Opgave 1 og 2 færdig

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Console.log() - Printopgaver
 
 // Opgave 1.1
-console.log("Hello World");
+console.log("Hello World")
 
 // Opgave 1.2
 console.log("Hello\nWorld")
@@ -10,11 +10,11 @@ console.log("Hello\nWorld")
 console.log("*\n**\n***\n**\n*")
 
 // Opgave 1.4
-let x = 5;
+let x = "5"
 console.log(typeof x)
 
 // Opgave 1.5
-console.log(10+5)
+console.log(10-5)
 console.log(25/3)
 console.log(25%3)
 console.log(25%2)
@@ -23,43 +23,34 @@ console.log(3**2)
 // Opgave 1.6
 let tekst1 = "Hello "
 let tekst2 = "World!"
-console.log(tekst1+tekst2)
+console.log(tekst1 + tekst2)
 
 // Opgave 1.7
-let tekst = "GF2 Online"
-console.log(tekst.length)
+let gf2Online = "GF2 Online"
+console.log(gf2Online.length)
 
 // Opgave 1.8
-console.log()
-
-// Opgave 1.9
 console.log(Math.random())
 
-// Opgave 1.10
+// Opgave 1.9
 let number = 23
-console.log(number) // viser tallet
-console.log(number = "23") // 23 er lig med 23
-console.log(number == "23") // bool (23 er lig med 23 = true)
-console.log(number === "23") // Tjekker om 23 er magen til 23
-console.log(number -= 23) // 23 - 23
-console.log(number += 23) // 23 + string 23
-console.log(number += "23") // Tjekker om 23 er magen til 23
-console.log(number -= "46") // 23 = 23 - 46
-console.log(number != "23") // 23 er ikke lig med string 23
-console.log(number **= 2) // 23 = 23 ** 2
+console.log(number) // viser tallet 23
+console.log(number = "23") // Ændring fra int til string
+console.log(number == "23") // Bool - tjekker om "23" er lig "23"
+console.log(number === "23") //
+console.log(number -= 23) //
+console.log(number += 23) //
+console.log(number += "23") //
+console.log(number -= "46") //
+console.log(number != "23") //
+console.log(number **= 2) //
 
-// ----------------------------------------------------------
 
-// Conditionals - If / Else statements
 
-// Opgave 2.1
-let newnumber = 5
 
-if (newnumber % 2 == 0){
-  console.log("Et lige tal")
-} else{
-  console.log("Et ulige tal")
-}
 
-// Opgave 2.2
+
+
+
+
 
