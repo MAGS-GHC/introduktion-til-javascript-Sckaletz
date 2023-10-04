f759d0bc3beee32a8d44330ab93eb83c37188812
Sckaletz <allan@leeloo.dk>
2023-10-04 11:48:30 +0200

    Update

---
+++ simpleloops.js
@@ -0,0 +1,13 @@
+// Opgave 3.1
+for (let i = 1; i <= 100; i++){
+    console.log(i)
+}
+
+// Opgave 3.2
+for (let col1 = 0; col1 <= 100; col1++) {
+    for (let col2 = 1; col2 <= 101; col2++){
+        for (let col3 = 2; col3 <= 102; col3++){
+            console.log(col1, col2, col3);
+        }
+    }
+}
