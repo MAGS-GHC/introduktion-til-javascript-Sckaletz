8e3547fc2719a9fc9654140c21f766d0e4e09da5
Sckaletz <allan@leeloo.dk>
2023-10-06 12:08:35 +0200

    Update

--- loops.js
+++ loops.js
@@ -24,10 +24,78 @@ function talIndex (talrækkeNummer){
     for (let index in talrækkeNummer){
         let højesteTalTalrække = Math.max(...talrækkeNummer);
         let højesteTalIndexTalrække = talrækkeNummer.indexOf(Math.max(...talrækkeNummer));
-        if (talrækkeNummer.length == Number(index)+1) {
+        if (talrækkeNummer.length === Number(index)+1) {
             console.log(`Højeste tal: ${højesteTalTalrække} Index: ${højesteTalIndexTalrække}`);
         }
     }
 }
-talIndex(talrække3);
-talIndex(talrække4);
+talIndex(talrække3); talIndex(talrække4);
+
+
+// Opgave 6.4
+let talrække5 = [0, 12, 0, 22, 300, 4, 5, 12, 12, 212, 12];
+let antalAf12Taller = 0;
+for (let index in talrække5) {
+    if (talrække5[index] === 12) {
+        antalAf12Taller++;
+    }
+    if (talrække5.length === Number(index)+1) {
+        console.log(`Antal 12 taller: ${antalAf12Taller}`);
+    }
+}
+
+
+// Opgave 6.5
+let talrække6 = [0, -12, 0, -22, -300, 4, 5, -12, 12, -212, 12]
+let antalPositiveTal = 0;
+for (let index in talrække6){
+    if (talrække6[index] > 0){
+        antalPositiveTal++;
+    }
+    if (talrække6.length === Number(index)+1) {
+        console.log(`Antal positive tal: ${antalPositiveTal}`);
+    }
+}
+
+
+// Opgave 6.6
+let talrække7 = [0, 12, 0, 22, 300, 4, 5, 1000, -50, 32];
+function gennemsnit(talrækkeListe){
+    let talrækkeSum = 0;
+    for (let index in talrækkeListe) {
+        talrækkeSum += talrækkeListe[index];
+        if (talrækkeListe.length === Number(index)+1) {
+            let talrække7Gennemsnit = talrækkeSum / talrækkeListe.length
+            console.log(talrække7Gennemsnit);
+        }
+    }
+}
+gennemsnit(talrække7);
+
+
+// Opgave 6.7
+
+
+
+// Opgave 6.8
+const person1 = {
+    firstName: "Anders",
+    lastName: "Andersen",
+    age: 29,
+    eyeColor: "grøn"
+};
+const person2 = {
+    firstName: "Mads",
+    lastName: "Madsen",
+    age: 32,
+    eyeColor: "blå"
+};
+const person3 = {
+    firstName: "Han",
+    lastName: "Hansen",
+    age: 55,
+    eyeColor: "brun"
+};
+if (age > 30 && eyeColor === "blå" && eyeColor === "grøn" && eyecolor === "rød"){
+    console.log(firstName, lastName);
+}
