75caa867694d57550bc31d42712bf6e505f6c959
Sckaletz <allan@leeloo.dk>
2023-10-06 08:19:31 +0200

    Update

---
+++ loops.js
@@ -0,0 +1,17 @@
+// Opgave 6.1
+let talrække = [0, 12, 0, 22, 300, 4, 5];
+let sum = 0;
+for (let i = 0; i < talrække.length; i++){
+    sum += talrække[i];
+}
+console.log(sum);
+
+
+// Opgave 6.2
+let talrække2 = [0, 12, 0, 22, 300, 4, 5];
+talrække2.forEach((tal) => {
+    console.log(tal);
+})
+
+
+// Opgave 6.3
